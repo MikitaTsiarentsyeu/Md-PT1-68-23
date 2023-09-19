8eb4e77601409e7302f235e5453afa4919798daf
KSENIYA1809 <94754990+KSENIYA1809@users.noreply.github.com>
2023-09-19 19:57:57 +0300

    add tasks

---
+++ Tasks/Surabka/Task_1/Task_1.py
@@ -0,0 +1,3 @@
+C = float(input('Please enter the Celsius: '))
+F = C * 1.8 + 32 
+print("the answer is", F)
---
+++ Tasks/Surabka/Task_1/Task_3.py
@@ -0,0 +1,3 @@
+speed = int(input('Please enter the speed: '))
+speed_2 = speed * 1000 / 3600
+print("the answer is", speed_2)
