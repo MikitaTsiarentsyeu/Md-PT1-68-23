1dc962fe2f1c6d850f3326762f18e802960d0561
MaximMG1991 <144839821+MaximMG1991@users.noreply.github.com>
2023-09-21 13:12:05 +0300

    Update Task1-4.py

--- Tasks/Myshkavets/Task1/Task1-4.py
+++ Tasks/Myshkavets/Task1/Task1-4.py
@@ -2,6 +2,6 @@
 
 exchange_rate = 3.25
 
-usd_amount = float(input("Enter amount in USD: "))
+usd_amount = int(input("Enter amount in USD: "))
 byn_amount = usd_amount * exchange_rate
 print(f"{usd_amount} USD is {byn_amount} BYN")
