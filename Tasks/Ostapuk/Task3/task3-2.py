97819721ae5a1cb43493b7a75842d51c42876ff0
Daniil <daniilostapuk2004@gmail.com>
2023-10-12 05:20:42 +0300

    add task3
    I almost figured out 3-10, I'll send it a little later)

---
+++ Tasks/Ostapuk/Task3/task3-2.py
@@ -0,0 +1,6 @@
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = 0
+for i in num:
+    if i % 2 == 0:
+        sum += i
+print(sum)
---
+++ Tasks/Ostapuk/Task3/task3-9.py
@@ -0,0 +1,3 @@
+string = input("enter the text: ")
+d_string = string[::-1]
+print(d_string)
