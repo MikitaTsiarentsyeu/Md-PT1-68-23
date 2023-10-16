35948fdd16d9400b2e7cef40366fd96562463574
SciPy <106493968+SciPy-data-scientist@users.noreply.github.com>
2023-10-16 20:43:27 +0300

    Modify task5-5.py using nested function

--- Tasks/Odnoburtsev/Task5/Task5-5.py
+++ Tasks/Odnoburtsev/Task5/Task5-5.py
@@ -9,18 +9,17 @@ raw_list.sort()
 # raw_list = [1, 2, 3, 4, 5, 7, 8, 9, 11, 13, 14, 17]
 
 def range_finder(x: list):
-    """Takes an ordered list of numbers and returns a string
+    """Takes an ordered list of numbers and returns a list of strings
     with ranges for those numbers"""
     
     print(f"A raw list of numbers is:\n{x}\n")
     counter = 0
     new_list = []
 
-    for i in range(len(x)-1):
-            
+    for i in range(len(x)-1):            
         if x[i+1] - x[i] == 1:
             counter += 1          
-            continue        
+            continue
         else:
             if counter == 0:
                 new_list.append(f"{x[i]}")
@@ -33,9 +32,15 @@ def range_finder(x: list):
     else:
         new_list.append(f"{x[i-counter+1]}-{x[i+1]}")
 
-    str = ", ".join(new_list)
-    print(repr(str))
-    
-    return str
+    def list_to_str(y: list):
+        """Takes a list of strings with ranges of numbers and
+        returns a string with ranges of numbers"""
 
-range_finder(raw_list)
+        str = ", ".join(y)
+        print(f"A string with ranges of numbers is:")
+        
+        return repr(str)
+    
+    return list_to_str(new_list)
+
+print(range_finder(raw_list))
